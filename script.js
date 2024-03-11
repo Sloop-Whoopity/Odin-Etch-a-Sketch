37fcd9b7d2f8e9306ee83b544a901bdf05e38762
elijah_ubuntu <elijah.pitt@proton.me>
2024-03-11 17:26:30 -0600

    Create 16 x 16 grid using for loop
    Created a 16 x 16 grid of squares by using a for loop to create
    16 containers and then a second for loop to create another 16 divs for
    each container.  This allowed me to create all necessary divs without
    copy and pasting by hand.

--- script.js
+++ script.js
@@ -1,55 +1,29 @@
 console.log("Welcome to the world beautiful")
 
 let body = document.querySelector("body");
-let container1 = document.createElement("div");
-let container2 = document.createElement("div");
-let container3 = document.createElement("div");
-let container4 = document.createElement("div");
 
+for (i = 1; i <= 16; i++) {
+    console.log("I am the loop!");
+    let container = document.createElement("div");
+    for (j = 1; j <= 16; j++) {
+        let item = document.createElement("div");
+        container.appendChild(item)
+    }
+    body.appendChild(container);
+}
 
-
-let div1 = document.createElement("div");
-let div2 = document.createElement("div");
-let div3 = document.createElement("div");
-let div4 = document.createElement("div");
-let div5 = document.createElement("div");
-let div6 = document.createElement("div");
-let div7 = document.createElement("div");
-let div8 = document.createElement("div");
-let div9 = document.createElement("div");
-let div10 = document.createElement("div");
-let div11 = document.createElement("div");
-let div12 = document.createElement("div");
-let div13 = document.createElement("div");
-let div14 = document.createElement("div");
-let div15 = document.createElement("div");
-let div16 = document.createElement("div");
-
-
-
-div1.textContent = "Where's my twin?"
-div2.textContent = "There she is!";
-div5.textContent = "What's up doc?"
-
-container1.append(div1, div2, div3, div4);
-body.append(container1, container2, container3, container4);
-container2.append(div5, div6, div7, div8);
-container3.append(div9, div10, div11, div12);
-container4.append(div13, div14, div15, div16);
 
 let indDivs = document.querySelectorAll("div div")
 let containers = document.querySelectorAll("body > div")
 
-container1.style.backgroundColor = "black";
-container2.style.backgroundColor = "orange";
-container3.style.backgroundColor = "purple";
-container4.style.backgroundColor = "red";
 
 body.style.height = "99vh";
+body.style.display = "flex";
 
 containers.forEach((element) => {
     element.style.display = "flex";
     element.style.flexGrow = "1";
+    element.style.backgroundColor = "red";
 })
 
 indDivs.forEach((element) => {
